32f97c2b7da62f306c1e2a625dcabc7f7e1c5e94
LeonSutedja <Leon.sutedja@gmail.com>
2016-04-03 15:05:09 +1000

    Added jquery dialog shopping cart
    Remove ProductOrdered page.
    Added jquery dialog shopping cart instead to process order, remove order
    quantity, or process order.
    
    Added jquery dialogExtend to allow the dialog to be minimizable

--- Shop/App_Start/BundleConfig.cs
+++ Shop/App_Start/BundleConfig.cs
@@ -9,7 +9,15 @@ namespace Shop
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/jquery-ui-{version}.js",
+                        "~/Scripts/respond.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                        "~/Scripts/bootstrap.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery-ui").Include(
+                        "~/Scripts/jquery-ui-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery-ui-dialogextend").Include(
                         "~/Scripts/jquery.dialogextend.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
@@ -20,25 +28,15 @@ namespace Shop
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
-
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css",
                       "~/Content/themes/base/core.css",
                       "~/Content/themes/base/resizable.css",
                       "~/Content/themes/base/selectable.css",
-                      "~/Content/themes/base/accordion.css",
-                      "~/Content/themes/base/autocomplete.css",
-                      "~/Content/themes/base/button.css",
                       "~/Content/themes/base/dialog.css",
-                      "~/Content/themes/base/slider.css",
-                      "~/Content/themes/base/tabs.css",
-                      "~/Content/themes/base/datepicker.css",
-                      "~/Content/themes/base/progressbar.css",
-                      "~/Content/themes/base/theme.css"));
+                      "~/Content/themes/base/theme.css",
+                      "~/Content/site.css"
+                      ));
             
         }
     }
---
+++ Shop/Shared/Components/ShoppingCartDialog/ShoppingCartDialogController.cs
@@ -0,0 +1,39 @@
+﻿using Shop.Infrastructure.Customer;
+using Shop.Infrastructure.Repository;
+using Shop.Order;
+using Shop.Session;
+using Shop.Shared.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shop.Shared.Components.ShoppingCartDialog
+{
+    public class ShoppingCartDialogController : BaseController
+    {
+        private IOrderService _orderService;
+
+        public ShoppingCartDialogController(IRepository<Customer> customerRepository, IOrderService orderService) : base(customerRepository)
+        {
+            _orderService = orderService;
+        }
+
+        [HttpPost]
+        public JsonResult ClearOrder()
+        {
+            SessionFacade.CurrentCustomerOrder(HttpContext, CurrentUser).Reset();
+            return Json(new { success = true });
+        }
+   
+        [HttpPost]
+        public JsonResult ProcessOrder()
+        {
+            var order = SessionFacade.CurrentCustomerOrder(HttpContext, CurrentUser).Get();
+            _orderService.ProcessOrder(order);
+            SessionFacade.CurrentCustomerOrder(HttpContext, CurrentUser).Reset();
+            return Json(new { success = true });
+        }
+    }
+}
