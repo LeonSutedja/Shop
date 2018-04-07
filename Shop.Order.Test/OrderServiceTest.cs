b5fb53def338606c86b53849e13a7c1fe66d8945
MOZART\Vivaldi <leon.sutedja@gmail.com>
2018-04-07 12:04:15 +1000

    Updated unity package in shop to the latest version. Re-organised Order.Test.

---
+++ Shop.Order.Test/OrderServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Shouldly;
+using Xunit;
+
+namespace Shop.Order.Test
+{
+    public class OrderServiceTest
+    {
+        public OrderServiceTest()
+        {
+            //    var unityContainer = new UnityContainer();
+
+            //    Shop.App_Start.UnityConfig.RegisterTypes(unityContainer as (Microsoft.Practices.Unity.UnityContainer));
+        }
+
+        [Fact]
+        public void PassingTest()
+        {
+            var a = 4;
+            a.ShouldBe(4);
+        }
+    }
+}
--- Shop/App_Start/UnityConfig.cs
+++ Shop/App_Start/UnityConfig.cs
@@ -1,4 +1,3 @@
-using Microsoft.Practices.Unity;
 using Shop.Infrastructure.Customer;
 using Shop.Infrastructure.Interfaces;
 using Shop.Infrastructure.Product;
@@ -9,47 +8,61 @@ using Shop.Shared.Models.CommandHandler;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Unity;
+using Unity.Lifetime;
+using Unity.RegistrationByConvention;
 
-namespace Shop.App_Start
+namespace Shop
 {
     /// <summary>
     /// Specifies the Unity configuration for the main container.
     /// </summary>
-    public class UnityConfig
+    public static class UnityConfig
     {
         #region Unity Container
 
-        private static Lazy<IUnityContainer> container = new Lazy<IUnityContainer>(() =>
-        {
-            var container = new UnityContainer();
-            RegisterTypes(container);
-            return container;
-        });
+        private static Lazy<IUnityContainer> container =
+          new Lazy<IUnityContainer>(() =>
+          {
+              var container = new UnityContainer();
+              RegisterTypes(container);
+              return container;
+          });
 
         /// <summary>
-        /// Gets the configured Unity container.
+        /// Configured Unity Container.
         /// </summary>
-        public static IUnityContainer GetConfiguredContainer()
-        {
-            return container.Value;
-        }
+        public static IUnityContainer Container => container.Value;
 
         #endregion Unity Container
 
-        /// <summary>Registers the type mappings with the Unity container.</summary>
+        /// <summary>
+        /// Registers the type mappings with the Unity container.
+        /// </summary>
         /// <param name="container">The unity container to configure.</param>
-        /// <remarks>There is no need to register concrete types such as controllers or API controllers (unless you want to
-        /// change the defaults), as Unity allows resolving a concrete type even if it was not previously registered.</remarks>
+        /// <remarks>
+        /// There is no need to register concrete types such as controllers or
+        /// API controllers (unless you want to change the defaults), as Unity
+        /// allows resolving a concrete type even if it was not previously
+        /// registered.
+        /// </remarks>
         public static void RegisterTypes(IUnityContainer container)
         {
+            // NOTE: To load from web.config uncomment the line below.
+            // Make sure to add a Unity.Configuration to the using statements.
+            // container.LoadConfiguration();
+
+            // TODO: Register your type's mappings here.
+            // container.RegisterType<IProductRepository, ProductRepository>();
+
             // NOTE: To load from web.config uncomment the line below. Make sure to add a Microsoft.Practices.Unity.Configuration to the using statements.
             // container.LoadConfiguration();
 
             /// Currently, we use singleton for repository. This is to handle function where object not being persisted into the database.
             /// The following line is to map between generic interface of IRepository to all of their implementations.
             container.RegisterTypes(AllClasses.FromLoadedAssemblies()
-                .Where(t => t.GetInterfaces().Any(i => i.IsGenericType &&
-                i.GetGenericTypeDefinition() == typeof(IRepository<>))),
+                    .Where(t => t.GetInterfaces().Any(i => i.IsGenericType &&
+                                                           i.GetGenericTypeDefinition() == typeof(IRepository<>))),
                 WithMappings.FromAllInterfaces,
                 WithName.Default, WithLifetime.ContainerControlled);
 
