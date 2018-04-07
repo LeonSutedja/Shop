e06eaffe784622423f943612804391d0e4c45a3f
MOZART\Vivaldi <leon.sutedja@gmail.com>
2018-04-07 11:41:07 +1000

    Added unit test for Order.Test project

---
+++ Shop.Order.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Shop.Order.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Shop.Order.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("4a380e5e-a2af-499c-b673-02ee45c7a7cf")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Shop.Order.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using Shouldly;
+using Xunit;
+
+namespace Shop.Order.Test
+{
+    public class Class1
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            var a = 4;
+            a.ShouldBe(4);
+        }
+    }
+}
