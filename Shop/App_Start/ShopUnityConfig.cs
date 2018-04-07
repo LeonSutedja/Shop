9ca43cb14c078200db23d0b90148d2ecfe389a23
MOZART\Vivaldi <leon.sutedja@gmail.com>
2018-04-07 17:13:35 +1000

    Move unity config to a different file.

---
+++ Shop/App_Start/ShopUnityConfig.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Shop.Infrastructure.Customer;
+using Shop.Infrastructure.Interfaces;
+using Shop.Infrastructure.Product;
+using Shop.Infrastructure.Repository;
+using Shop.Infrastructure.TableCreator;
+using Shop.Order;
+using Shop.Shared.Models.CommandHandler;
+using Unity;
+using Unity.Lifetime;
+using Unity.RegistrationByConvention;
+
+namespace Shop
+{
+    public static class ShopUnityConfig
+    {
+        public static void RegisterTypes(IUnityContainer container)
+        {
+            container.RegisterTypes(AllClasses.FromLoadedAssemblies()
+                    .Where(t => t.GetInterfaces().Any(i => i.IsGenericType &&
+                                                           i.GetGenericTypeDefinition() == typeof(IRepository<>))),
+                WithMappings.FromAllInterfaces,
+                WithName.Default, WithLifetime.ContainerControlled);
+
+            // External services mapping
+            // Map shop.infrastructure.service
+            container.RegisterType<IProductService, ProductService>(new ContainerControlledLifetimeManager());
+            container.RegisterType<ICustomerService, CustomerService>(new ContainerControlledLifetimeManager());
+
+            // Map Shop.Order
+            container.RegisterType<IOrderService, OrderService>(new ContainerControlledLifetimeManager());
+
+            _registerCommandHandlers(container);
+            _registerTableCreator(container);
+        }
+
+        private static void _registerTableCreator(IUnityContainer container)
+        {
+            // TableCreator
+            container.RegisterTypes(AllClasses.FromLoadedAssemblies()
+                    .Where(t => t.GetInterfaces().Any(i => i.IsGenericType &&
+                                                           i.GetGenericTypeDefinition() == typeof(ITableColumnRepository<>))),
+                WithMappings.FromAllInterfaces,
+                WithName.Default, WithLifetime.PerThread);
+
+            var tColumnTypes = AllClasses.FromLoadedAssemblies()
+                .Where(t => t.GetInterfaces().Any(i => i.IsGenericType &&
+                                                       i.GetGenericTypeDefinition() == typeof(ITableColumn<>))).ToList();
+
+            container.RegisterTypes(tColumnTypes,
+                WithMappings.FromAllInterfaces,
+                WithName.TypeName, WithLifetime.PerThread);
+
+            var ienumerable = typeof(IEnumerable<>);
+            var itableColumnType = typeof(ITableColumn<Product>);
+            Type[] typeArgs = { itableColumnType };
+            ienumerable.MakeGenericType(typeArgs);
+
+            container.RegisterType(typeof(IEnumerable<ITableColumn>), typeof(ITableColumn[]));
+
+            container.RegisterTypes(AllClasses.FromLoadedAssemblies()
+                    .Where(t => t.GetInterfaces().Any(i => i.IsGenericType &&
+                                                           i.GetGenericTypeDefinition() == typeof(ITableBuilder<>))),
+                WithMappings.FromAllInterfaces,
+                WithName.Default, WithLifetime.PerThread);
+        }
+
+        private static void _registerCommandHandlers(IUnityContainer container)
+        {
+            // Handlers
+            container.RegisterType<ICommandHandlerFactory, CommandHandlerFactory>(new ContainerControlledLifetimeManager());
+            container.RegisterTypes(AllClasses.FromLoadedAssemblies()
+                    .Where(t => t.GetInterfaces().Any(i => i.IsGenericType &&
+                                                           i.GetGenericTypeDefinition() == typeof(ICommandHandler<,>))), WithMappings.FromAllInterfaces,
+                WithName.Default, WithLifetime.ContainerControlled);
+        }
+    }
+}
--- Shop/App_Start/UnityConfig.cs
+++ Shop/App_Start/UnityConfig.cs
@@ -1,16 +1,5 @@
-using Shop.Infrastructure.Customer;
-using Shop.Infrastructure.Interfaces;
-using Shop.Infrastructure.Product;
-using Shop.Infrastructure.Repository;
-using Shop.Infrastructure.TableCreator;
-using Shop.Order;
-using Shop.Shared.Models.CommandHandler;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using Unity;
-using Unity.Lifetime;
-using Unity.RegistrationByConvention;
 
 namespace Shop
 {
@@ -58,65 +47,7 @@ namespace Shop
             // NOTE: To load from web.config uncomment the line below. Make sure to add a Microsoft.Practices.Unity.Configuration to the using statements.
             // container.LoadConfiguration();
 
-            /// Currently, we use singleton for repository. This is to handle function where object not being persisted into the database.
-            /// The following line is to map between generic interface of IRepository to all of their implementations.
-            container.RegisterTypes(AllClasses.FromLoadedAssemblies()
-                    .Where(t => t.GetInterfaces().Any(i => i.IsGenericType &&
-                                                           i.GetGenericTypeDefinition() == typeof(IRepository<>))),
-                WithMappings.FromAllInterfaces,
-                WithName.Default, WithLifetime.ContainerControlled);
-
-            // External services mapping
-            // Map shop.infrastructure.service
-            container.RegisterType<IProductService, ProductService>(new ContainerControlledLifetimeManager());
-            container.RegisterType<ICustomerService, CustomerService>(new ContainerControlledLifetimeManager());
-
-            // Map Shop.Order
-            container.RegisterType<IOrderService, OrderService>(new ContainerControlledLifetimeManager());
-
-            _registerCommandHandlers(container);
-            _registerTableCreator(container);
-        }
-
-        private static void _registerTableCreator(IUnityContainer container)
-        {
-            // TableCreator
-            container.RegisterTypes(AllClasses.FromLoadedAssemblies()
-                    .Where(t => t.GetInterfaces().Any(i => i.IsGenericType &&
-                                                           i.GetGenericTypeDefinition() == typeof(ITableColumnRepository<>))),
-                WithMappings.FromAllInterfaces,
-                WithName.Default, WithLifetime.PerThread);
-
-            var tColumnTypes = AllClasses.FromLoadedAssemblies()
-                .Where(t => t.GetInterfaces().Any(i => i.IsGenericType &&
-                                                       i.GetGenericTypeDefinition() == typeof(ITableColumn<>))).ToList();
-
-            container.RegisterTypes(tColumnTypes,
-                WithMappings.FromAllInterfaces,
-                WithName.TypeName, WithLifetime.PerThread);
-
-            var ienumerable = typeof(IEnumerable<>);
-            var itableColumnType = typeof(ITableColumn<Product>);
-            Type[] typeArgs = { itableColumnType };
-            ienumerable.MakeGenericType(typeArgs);
-
-            container.RegisterType(typeof(IEnumerable<ITableColumn>), typeof(ITableColumn[]));
-
-            container.RegisterTypes(AllClasses.FromLoadedAssemblies()
-                    .Where(t => t.GetInterfaces().Any(i => i.IsGenericType &&
-                                                           i.GetGenericTypeDefinition() == typeof(ITableBuilder<>))),
-                WithMappings.FromAllInterfaces,
-                WithName.Default, WithLifetime.PerThread);
-        }
-
-        private static void _registerCommandHandlers(IUnityContainer container)
-        {
-            // Handlers
-            container.RegisterType<ICommandHandlerFactory, CommandHandlerFactory>(new ContainerControlledLifetimeManager());
-            container.RegisterTypes(AllClasses.FromLoadedAssemblies()
-                    .Where(t => t.GetInterfaces().Any(i => i.IsGenericType &&
-                                                           i.GetGenericTypeDefinition() == typeof(ICommandHandler<,>))), WithMappings.FromAllInterfaces,
-                WithName.Default, WithLifetime.ContainerControlled);
+            ShopUnityConfig.RegisterTypes(container);
         }
     }
 }
