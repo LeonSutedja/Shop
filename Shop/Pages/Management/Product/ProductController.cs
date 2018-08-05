0d616c7bacdfe767e589a774a274cba41df4fc4e
MOZART\Vivaldi <leon.sutedja@gmail.com>
2018-08-05 16:43:42 +1000

    Added interface for sort by and sort direction.

--- Shop/Pages/Management/Product/ProductController.cs
+++ Shop/Pages/Management/Product/ProductController.cs
@@ -1,23 +1,29 @@
-﻿using Shop.Infrastructure.Customer;
+﻿using System;
+using System.Linq;
+using Shop.Infrastructure.Customer;
 using Shop.Infrastructure.Interfaces;
 using Shop.Infrastructure.Repository;
 using Shop.Infrastructure.TableCreator;
 using Shop.Shared.Controllers;
 using Shop.Shared.Models.CommandHandler;
 using System.Web.Mvc;
+using Shop.Infrastructure.TableCreator.Column;
 
 namespace Shop.Pages.Management.Product
 {
     public class ProductController : BaseController
     {
+        private readonly ITableColumnRepository<Infrastructure.Product.Product> _productColumnRepository;
         private readonly IProductService _productService;
         private readonly ICommandHandlerFactory _commandHandlerFactory;
 
         public ProductController(
+            ITableColumnRepository<Infrastructure.Product.Product> productColumnRepository,
             IProductService productService,
             IRepository<Customer> customerRepository,
             ICommandHandlerFactory commandHandlerFactory) : base(customerRepository)
         {
+            _productColumnRepository = productColumnRepository;
             _productService = productService;
             _commandHandlerFactory = commandHandlerFactory;
         }
@@ -26,19 +32,34 @@ namespace Shop.Pages.Management.Product
         /// Landing Page
         /// </summary>
         /// <returns></returns>
-        public ActionResult Index()
+        public ActionResult Index(string sortBy, string sortDirection)
         {
+            TableColumnIdentifier sortByidentifier = null;
+            var sortDirectionAsc = true;
+            if (sortBy != null)
+            {
+                var allProductViewColumns = _productColumnRepository.GetAllViewColumns();
+                sortByidentifier = allProductViewColumns
+                    .First(
+                        col => col.GetColumnDefinition().Identifier.AdditionalData == sortBy)
+                    .GetColumnDefinition().Identifier;
+            }
+
+            if (sortDirection != null)
+                sortDirectionAsc = String.CompareOrdinal(sortDirection, "desc") != 0;
+
             var tableInput = new TableInput
             {
                 PageNumber = 1,
                 PageSize = 10,
-                SortDirectionAsc = true
+                SortDirectionAsc = sortDirectionAsc,
+                SortBy = sortByidentifier
             };
 
             var tableOutput = _productService.GetProducts(tableInput);
             return View(tableOutput);
         }
-
+        
         [HttpPost]
         public ActionResult UpdateProduct(ProductEditViewModel viewModel)
         {
